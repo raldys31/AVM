a93c1f15b0b5c1b48b923aba0fea94ec167c98af
raldys.cruz <raldys.cruz@upr.edu>
2020-03-14 01:24:15 -0400

    Added score to candidate class.

--- src/Candidate.java
+++ src/Candidate.java
@@ -1,15 +1,13 @@
 public class Candidate {
 
-	public static void main(String[] args) {
-
-	}
-
 	public String name;
 	public int candidateID;
+	public int score;
 
 	public Candidate(String name, int candidatenumber) {
 		this.name = name;
 		this.candidateID = candidatenumber;
+		this.score = 0;
 	}
 
 	public String getName() {
@@ -19,6 +17,18 @@ public class Candidate {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public void addScore() {
+		this.score++;
+	}
 
 	public int getCandidateID() {
 		return candidateID;
@@ -27,6 +37,5 @@ public class Candidate {
 	public void setCandidateID(int candidatenumber) {
 		this.candidateID = candidatenumber;
 	}
-
 
 }
